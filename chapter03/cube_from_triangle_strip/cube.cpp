8fd0cbf0c818728ecd32e607d61df774a5587880
Colin Xu <colin.xu@gmail.com>
2016-02-15 16:57:53 +0800

    Draw cube from triangle strips using different methods:   - Separate draws with two sets of vertices indices, each set represent   a triangle strip.   - One draw with one set of vertices indices, using primitive restart.
    Signed-off-by: Colin Xu <colin.xu@gmail.com>

---
+++ chapter03/cube_from_triangle_strip/cube.cpp
@@ -0,0 +1,199 @@
+#include <iostream>
+#undef __WIN32
+#include "vgl.h"
+#include "vmath.h"
+#include "LoadShaders.h"
+#include "GL/freeglut.h"
+
+using namespace std;
+using namespace vmath;
+
+GLuint VAOs[1]; // Vertex Array Object
+GLuint IBOs[1]; // Index Buffer Object
+GLuint VBOs[1]; // Vertex Buffer Object
+
+GLuint gProgram = 0;
+
+ShaderInfo gShaders[] = {
+        { GL_VERTEX_SHADER, "vertex_shader.glsl" },
+        { GL_FRAGMENT_SHADER, "fragment_shader.glsl" },
+        { GL_NONE, NULL }
+    };
+
+// 8 vertices
+static const GLfloat cube_vertices[] =
+{
+    -1.0f, -1.0f, -1.0f, 1.0f,
+    -1.0f, -1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f, -1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f, 1.0f,
+    1.0f, -1.0f, -1.0f, 1.0f,
+    1.0f, -1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f, -1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f, 1.0f
+};
+
+// Color for each vertex
+static const GLfloat cube_vertex_colors[] =
+{
+    1.0f, 1.0f, 1.0f, 1.0f,
+    1.0f, 0.0f, 0.0f, 1.0f,
+    0.0f, 1.0f, 0.0f, 1.0f,
+    0.0f, 0.0f, 1.0f, 1.0f,
+    1.0f, 1.0f, 0.0f, 1.0f,
+    1.0f, 0.0f, 1.0f, 1.0f,
+    0.0f, 1.0f, 1.0f, 1.0f,
+    0.0f, 0.0f, 0.0f, 1.0f
+};
+
+// Indices for the triangle strips
+static const GLushort cube_indices[] =
+{
+    0, 1, 2, 3, 6, 7, 4, 5, // First strip
+    0xFFFF, // <<- - This is the restart index
+    2, 6, 0, 4, 1, 5, 3, 7 // Second strip
+};
+
+float aspect;
+GLint render_model_matrix_loc;
+GLint render_projection_matrix_loc;
+//---------------------------------------------------------------------
+//
+// init
+//
+void
+init(void)
+{
+    // Load shaders
+    gProgram = LoadShaders(gShaders);
+    
+    // Use shader program
+    glUseProgram(gProgram);
+    
+    render_model_matrix_loc = glGetUniformLocation(gProgram, "model_matrix");
+    render_projection_matrix_loc = glGetUniformLocation(gProgram, "projection_matrix");
+
+    // Create VAO, bind VAO
+    glGenVertexArrays(1, VAOs);
+    glBindVertexArray(VAOs[0]);
+    
+    // Create IBO, bind IBO, upload vertex index to IBO
+    glGenBuffers(1, IBOs);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBOs[0]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_indices), cube_indices, GL_STATIC_DRAW);
+    
+    // Create VBO, bind VBO, upload vertex position and color to VBO
+    glGenBuffers(1, VBOs);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices) + sizeof(cube_vertex_colors), NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(cube_vertices), cube_vertices);
+    glBufferSubData(GL_ARRAY_BUFFER, sizeof(cube_vertices), sizeof(cube_vertex_colors), cube_vertex_colors);
+    
+    // Setup VAO
+    glVertexAttribPointer(glGetAttribLocation(gProgram, "position"), 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+    glVertexAttribPointer(glGetAttribLocation(gProgram, "color"), 4, GL_FLOAT, GL_FALSE, 0, (const GLvoid *)sizeof(cube_vertices));
+    glEnableVertexAttribArray(glGetAttribLocation(gProgram, "position"));
+    glEnableVertexAttribArray(glGetAttribLocation(gProgram, "color"));
+    
+    glClearColor(0, 0, 0, 1);
+    
+    // Unbind VAO, IBO,  VBO
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+//---------------------------------------------------------------------
+//
+// display
+//
+void
+display(void)
+{
+    bool auto_redraw = true;
+    static const vmath::vec3 X(1.0f, 0.0f, 0.0f);
+    static const vmath::vec3 Y(0.0f, 1.0f, 0.0f);
+    static const vmath::vec3 Z(0.0f, 0.0f, 1.0f);
+    
+    glEnable(GL_PRIMITIVE_RESTART);
+    glEnable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
+    
+    // Clear screen
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Use shader program
+    glUseProgram(gProgram);
+       
+    // Bind VAO
+    glBindVertexArray(VAOs[0]);
+    
+    // Set up the model and projection matrix
+    float t = float(GetTickCount() & 0x1FFF) / float(0x1FFF);
+    vmath::mat4 model_matrix;
+    vmath::mat4 projection_matrix(vmath::frustum(-1.0f, 1.0f, -aspect, aspect, 1.0f, 500.0f));
+    glUniformMatrix4fv(render_projection_matrix_loc, 1, GL_FALSE, projection_matrix);
+    
+    // Draw cube with two triangle strips
+    model_matrix = vmath::translate(-1.5f, 0.0f, -5.0f) * rotate(t * 360.0f, Y) * rotate(t * 720.0f, Z);
+    glUniformMatrix4fv(render_model_matrix_loc, 1, GL_FALSE, model_matrix);
+    glDrawElements(GL_TRIANGLE_STRIP, 8, GL_UNSIGNED_SHORT, NULL);
+    glDrawElements(GL_TRIANGLE_STRIP, 8, GL_UNSIGNED_SHORT, (const GLvoid *)(9 * sizeof(GLushort)));
+    
+    // Draw cube with primitive restart
+    model_matrix = vmath::translate(1.5f, 0.0f, -5.0f) * rotate(t * 360.0f, Y) * rotate(t * 720.0f, Z);
+    glUniformMatrix4fv(render_model_matrix_loc, 1, GL_FALSE, model_matrix);
+    glPrimitiveRestartIndex(0xFFFF);
+    glDrawElements(GL_TRIANGLE_STRIP, 17, GL_UNSIGNED_SHORT, NULL);
+    
+    glutSwapBuffers();
+    if (auto_redraw)
+    {
+        glutPostRedisplay();
+    }
+    
+    // Unbind VAO and shader program after usage
+    glBindVertexArray(0);
+    glUseProgram(0);
+}
+
+//---------------------------------------------------------------------
+//
+// reshape
+//
+void reshape(int width, int height)
+{
+    glViewport(0, 0 , width, height);
+
+    aspect = float(height) / float(width);
+}
+//---------------------------------------------------------------------
+//
+// main
+//
+int
+main(int argc, char** argv)
+{
+    glewExperimental = GL_TRUE;
+
+#ifdef _DEBUG
+        glutInitContextFlags(GLUT_DEBUG);
+#endif
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition (140, 140);
+    glutInitContextVersion(4, 3);
+    glutInitContextProfile(GLUT_CORE_PROFILE);
+    glutCreateWindow(argv[0]);
+    if (glewInit()) {
+        cerr << "Unable to initialize GLEW ... exiting" << endl;
+        exit(EXIT_FAILURE);
+    }
+    init();
+    
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+
+    glutMainLoop();
+}
